1db1e5f953692dfb4a4953551fec3fa32a68a1f7
felixabel684 <felixabel684@gmail.com>
2024-04-25 08:10:29 +0700

    membuat website kedai kopi

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// klik di luar sidebar utk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.targer) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
